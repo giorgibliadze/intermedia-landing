b8ef53cb5916ece834584ac6daa033d8e33aa084
giorgibliadze <bliadze1997@gmail.com>
2025-06-01 13:32:21 +0400

    icon links update

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { User } from 'lucide-react';
+"use client";
+
+import Link from "next/link";
+import { User } from "lucide-react";
 
 const MobileUserIcon = () => {
-    return (
-        <>
-            <div className="flex md:hidden justify-center items-center w-[36px] h-[36px] border border-[#E5E5E5] rounded-full relative">
-                <User className="w-[15px] h-[15px] text-black" />
-            </div>
-            <User className="hidden md:flex w-[15px] h-[15px] text-black" />
-        </>
-    );
+  return (
+    <Link href="/login" aria-label="Go to login">
+      <div className="flex md:hidden justify-center items-center w-[36px] h-[36px] border border-[#E5E5E5] rounded-full relative">
+        <User className="w-[15px] h-[15px] text-black" />
+      </div>
+      <User className="hidden md:flex w-[15px] h-[15px] text-black" />
+    </Link>
+  );
 };
 
 export default MobileUserIcon;
--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -2,20 +2,25 @@
 
 import React from 'react';
 import { Search } from 'lucide-react';
+import Link from 'next/link';
 
 const SearchBar = () => {
     return (
         <>
             {/* Mobile version: visible only on small screens */}
-            <div className="md:hidden flex items-center gap-2 px-4 h-8 border border-white rounded-full text-black text-sm font-normal">
-                <Search className="w-4 h-4 text-black" />
-                <span>მოძებნე</span>
-            </div>
+            <Link href="/search" aria-label="Go to login">
+                <div className="md:hidden flex items-center gap-2 px-4 h-8 border border-white rounded-full text-black text-sm font-normal">
+                    <Search className="w-4 h-4 text-black" />
+                    <span>მოძებნე</span>
+                </div>
+            </Link>
 
             {/* Desktop version: visible from md and up */}
-            <button aria-label="Open search" className="hidden md:flex w-10 h-10 justify-center items-center rounded-full hover:bg-gray-100 transition">
-                <Search className="w-5 h-5 text-gray-700" />
-            </button>
+            <Link href="/search" aria-label="Go to login">
+                <button aria-label="Open search" className="hidden md:flex w-10 h-10 justify-center items-center rounded-full hover:bg-gray-100 transition">
+                    <Search className="w-5 h-5 text-gray-700" />
+                </button>
+            </Link>
         </>
     );
 };
